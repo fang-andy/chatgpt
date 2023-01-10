6066a6dc637a9b4419c9d98e318492d4cf59dace
fang-andy <afang1094@gmail.com>
2023-01-10 08:21:00 -0800

    updated header component and updated navbar classnames

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import './header.css'
+import React from 'react';
+import './header.css';
+import people from '../../assets/people.png';
+import ai from '../../assets/ai.png';
 
 const Header = () => {
     return (
-        <div>
-            Header
+        <div className="gpt__header section__padding" id="home">
+            <div className="gpt__header-content">
+                <h1 className="gradient__text">Let's Build Something amazing with Chat GPT Open AI</h1>
+                <p>I'm a wangsta, I'm a straight up G. The gangsta life is the life for me</p>
+
+                <div className="gpt__header-content__input">
+                    <input type="email" placeholder="Your email Address"></input>
+                    <button type="button">Get Started</button>
+                </div>
+
+                <div className="gpt__header-content__people">
+                    <img src={people} alt="people" />
+                    <p>1,600 people requested access</p>
+                </div>
+
+            </div>
+            <div className="gpt__header-image">
+                <img src={ai} alt="ai" />
+            </div>
         </div>
     )
 }
