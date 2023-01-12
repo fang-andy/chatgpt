f9c7b11c947a994bd479e7e5467cfc9a8c54c03a
fang-andy <afang1094@gmail.com>
2023-01-12 08:21:53 -0800

    updated call to action component

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import './cta.css'
+import React from 'react';
+import './cta.css';
 
 const CTA = () => {
     return (
-        <div>
-            CTA
+        <div className="gpt__cta">
+            <div className="gpt__cta-content">
+                <p>Request early Access to Get Started</p>
+                <h3>Register today & start exploring the endless possibilities.</h3>
+            </div>
+            <div className="gpt__cta-btn">
+                <button type="button">Get Started</button>
+            </div>
         </div>
     )
 }
